fbaa7c7cda64d756357848338dd16c1e86ee1aca
Thanhokjztr <phamtanthanh17a@gmail.com>
2024-08-28 16:49:42 +0700

    update

---
+++ JAVACRIP/amthuc.js
@@ -0,0 +1,23 @@
+const images = [
+    '../IMAGES/Amthuc.webp',
+    '..//IMAGES/Amthuc2.webp',
+    '..//IMAGES/Amthuc3.webp'
+];
+
+let tmp = 0;
+const Hinhnen = document.getElementById('background');
+
+function Thaydoinen() {
+    Hinhnen.style.backgroundImage = `url(${images[tmp]})`;
+    tmp = tmp + 1;
+    if(tmp===3)
+        tmp=0;
+}
+
+// Thay đổi hình nền mỗi 5 giây (5000 milliseconds)
+setInterval(Thaydoinen, 3000);
+
+// Đặt hình nền ban đầu
+Thaydoinen();
+
+// Hình nền
